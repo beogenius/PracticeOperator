8633656d70676a4c2687de5c89a87b51d864af66
beogenius <70867972+beogenius@users.noreply.github.com>
2020-09-10 08:36:35 +0700

    Thuc hanh toan tu

---
+++ js.js
@@ -0,0 +1,12 @@
+let inputWidth;
+let inputHeight;
+
+inputWidth = prompt('Enter the width');
+inputHeight = prompt('Enter the Height');
+
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+
+let area = width * height ;
+document.write("the area is " + area);
+
